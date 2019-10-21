b7c6e7fb2df4ec4cd29088a064b486d742a25964
Erik Benton <erik.benton@hotmail.com>
2019-10-21 12:26:41 -0400

    completing filter_lengths

--- Software Engineering Foundations/Advanced Methods/Exercises/advanced_methods_exercise/04_filter_lengths.rb
+++ Software Engineering Foundations/Advanced Methods/Exercises/advanced_methods_exercise/04_filter_lengths.rb
@@ -3,6 +3,9 @@
 # that have at least the given length. The length argument should be optional; if no length
 # is passed in, then 5 should be used as the length.
 
+def filter_lengths(arr, num=5)
+	return arr.select { |str| str.length >= num }
+end
 
 p filter_lengths(["pear", "dragonfruit", "fig", "clementine"], 4)   # => ["pear", "dragonfruit", "clementine"]
 p filter_lengths(["pear", "dragonfruit", "fig", "clementine"])      # => ["dragonfruit", "clementine"]
