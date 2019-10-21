0dd7385de32512d14dc12689d96a7ea18ab10f28
Erik Benton <erik.benton@hotmail.com>
2019-10-21 12:11:35 -0400

    finishing coprime problem

--- Software Engineering Foundations/Advanced Methods/Exercises/advanced_methods_exercise/01_coprime.rb
+++ Software Engineering Foundations/Advanced Methods/Exercises/advanced_methods_exercise/01_coprime.rb
@@ -3,6 +3,30 @@
 # The method should return false otherwise. For example coprime?(25, 12) is true because
 # 1 is the only number that divides both 25 and 12.
 
+# Resposible for getting the (non-obvious) factors
+# of the given number and returning an array
+def get_factors(number)
+	# Select the entries that evenly divide input
+	res = (2...number).select { |num| number % num == 0 }
+	return res
+end
+
+def coprime?(num1, num2)
+	# Get the factors for both the numbers
+	factors1 = get_factors(num1)
+	factors2 = get_factors(num2)
+	# Nested loops to compare each entry in factor arrays
+	factors1.each do |factor1|
+		factors2.each do |factor2|
+			# If there is a common factor
+			if factor1 == factor2
+				return false
+			end
+		end
+	end
+	# No common factors found
+	return true
+end
 
 p coprime?(25, 12)    # => true
 p coprime?(7, 11)     # => true
