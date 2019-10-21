7e320fda7ccd6f005814db92a45251be36cccf51
Erik Benton <erik.benton@hotmail.com>
2019-10-21 15:32:25 -0400

    completing bubble_sort

--- Software Engineering Foundations/Algorithm Strategies/Exercises/algorithmic_strategies_exercise/03_bubble_sort.rb
+++ Software Engineering Foundations/Algorithm Strategies/Exercises/algorithmic_strategies_exercise/03_bubble_sort.rb
@@ -3,6 +3,19 @@
 # The method should return the array.
 # Do not use the built-in Array#sort
 
+def bubble_sort(arr)
+  swap = true
+  while swap
+    swap = false
+    (0...arr.length - 1).each do |idx|
+      if arr[idx] > arr[idx + 1]
+        arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
+        swap = true
+      end
+    end
+  end
+  return arr
+end
 
 p bubble_sort([2, 8, 5, 2, 6])      # => [2, 2, 5, 6, 8]
 p bubble_sort([10, 8, 7, 1, 2, 3])  # => [1, 2, 3, 7, 8, 10]
