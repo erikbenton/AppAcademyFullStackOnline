1f20bb755a5af83c07360bc8d1447dbd6ed2a6cb
Erik Benton <erik.benton@hotmail.com>
2019-10-21 15:25:46 -0400

    completing swapper

--- Software Engineering Foundations/Algorithm Strategies/Exercises/algorithmic_strategies_exercise/01_swapper.rb
+++ Software Engineering Foundations/Algorithm Strategies/Exercises/algorithmic_strategies_exercise/01_swapper.rb
@@ -2,6 +2,10 @@
 # The method should swap the elements at the given indices and return the array.
 # The method should modify the existing array and not create a new array.
 
+def swapper(arr, idx1, idx2)
+  arr[idx1], arr[idx2] = arr[idx2], arr[idx1]
+  arr
+end
 
 p swapper(["a", "b", "c", "d"], 0, 2)               # => ["c", "b", "a", "d"]
 p swapper(["a", "b", "c", "d"], 3, 1)               # => ["a", "d", "c", "b"]
