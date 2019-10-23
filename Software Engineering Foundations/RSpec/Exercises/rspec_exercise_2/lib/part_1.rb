761d05fd7bdf000ecd00d0d0338341e5b6f3b002
Erik Benton <erik.benton@hotmail.com>
2019-10-23 08:46:58 -0400

    finishing rspec exercise part 1

---
+++ Software Engineering Foundations/RSpec/Exercises/rspec_exercise_2/lib/part_1.rb
@@ -0,0 +1,35 @@
+def partition(arr, num)
+  smaller = arr.select { |ele| ele < num }
+  larger = arr.select { |ele| ele >= num }
+  [smaller, larger]
+end
+
+def merge(hash_1, hash_2)
+  res = hash_1.dup
+  hash_2.each { |key, val| res[key] = val }
+  res
+end
+
+def censor(sentence, curse_words)
+  vowels = "aeiou".split("")
+  words = sentence.split.map do |word|
+    if curse_words.include?(word.downcase)
+      word.each_char.with_index do |char, idx|
+        if vowels.include?(char.downcase)
+          word[idx] = "*"
+        end
+      end
+    end
+    word
+  end
+  words.join(" ")
+end
+
+def power_of_two?(num)
+  power = 1
+  while power <= num
+    return true if power == num
+    power *= 2
+  end
+  false
+end
