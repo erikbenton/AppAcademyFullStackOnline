182fdf97b89da5f7bed0509d085f2f5b7e6fc84f
erikbenton <erik.benton@hotmail.com>
2019-10-26 07:51:43 -0400

    changing logic in fuzz_buzz

--- Software Engineering Foundations/Debugging/Exercises/debugging_exercise_1/debugging_exercise_1/lib/fuzz_buzz.rb
+++ Software Engineering Foundations/Debugging/Exercises/debugging_exercise_1/debugging_exercise_1/lib/fuzz_buzz.rb
@@ -6,12 +6,15 @@
 require "byebug"
 
 # Had to change the if: || => &&
+# Post-solutions
+#   Changed logic to more accurately represent docs:
+#       "not divisible by 3 or 5"
 
 def fuzz_buzz(num)
     numbers = []
 
     (1..num).each do |i|
-        if i % 3 != 0 && i % 5 != 0
+        if !(i % 3 == 0 || i % 5 == 0)
             numbers << i
         end
     end
