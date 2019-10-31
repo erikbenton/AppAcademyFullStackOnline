632208e188743a7544a02f1245c98300c2bf2ea2
Erik Benton <erik.benton@hotmail.com>
2019-10-31 08:05:01 -0400

    finishing abstraction & encapsulation notes

--- Software Engineering Foundations/Object Oriented Programming/Notes/abstraction_encapsulation.rb
+++ Software Engineering Foundations/Object Oriented Programming/Notes/abstraction_encapsulation.rb
@@ -36,3 +36,38 @@ course.enroll("Erik")
 p course
 
 # Encapsulation
+#   Hiding the inner data from the end user
+#   While still allowing them to use the information
+
+class Queue
+  def initialize
+    @line = []
+  end
+
+  def add(ele)
+    @line << ele
+    nil
+  end
+
+  def remove
+    @line.shift
+  end
+end
+
+grocery_checkout = Queue.new
+grocery_checkout.add("bob")
+grocery_checkout.add("alice")
+grocery_checkout.remove          # => "bob"
+grocery_checkout.add("noam")
+grocery_checkout.add("donald")
+grocery_checkout.remove          # => "alice"
+grocery_checkout.remove          # => "noam"
+grocery_checkout.remove          # => "donald"
+
+p grocery_checkout
+
+# Leave @line hidden from the user so that can't access it like:
+# grocery_checkout.add("bob")
+# grocery_checkout.add("alice")
+# grocery_checkout.line.unshift("noam")   # noam cut the line!
+# grocery_checkout.remove                 # => "noam"
