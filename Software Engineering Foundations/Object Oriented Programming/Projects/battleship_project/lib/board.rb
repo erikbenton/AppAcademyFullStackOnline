4ea553abc750d90ead5995e902a80fd8d39c7f01
erikbenton <erik.benton@hotmail.com>
2019-11-02 19:07:20 -0400

    making small changes after watching the solutions

--- Software Engineering Foundations/Object Oriented Programming/Projects/battleship_project/lib/board.rb
+++ Software Engineering Foundations/Object Oriented Programming/Projects/battleship_project/lib/board.rb
@@ -1,22 +1,22 @@
 class Board
 
   attr_reader :size
+  
+  def self.create_grid(rows, cols, filler)
+    Array.new(rows) { Array.new(cols, filler) }
+  end
+  
+  def self.print_grid(grid)
+    grid.each do |row|
+      puts row.join(" ")
+    end
+  end
 
   def initialize(n)
     @grid = Board.create_grid(n, n, :N)
     @size = n * n
   end
-
-  def self.create_grid(rows, cols, filler)
-    res = []
-    rows.times do
-      sub_res = []
-      cols.times { sub_res << filler }
-      res << sub_res
-    end
-    res
-  end
-
+  
   def [](pos)
     row, col = pos
     @grid[row][col]
@@ -43,33 +43,50 @@ class Board
     end
   end
 
+  # def place_random_ships
+  #   spots = @size / 4
+  #   new_ships = []
+  #   spots.times do
+  #     new_ship = self.get_random_position
+  #     until !new_ships.include?(new_ship)
+  #       new_ship = self.get_random_position
+  #     end
+  #     new_ships << new_ship
+  #     self[new_ship] = :S
+  #   end
+  # end
+
+  def get_random_position
+    [rand(0...@grid.length), rand(0...@grid.length)]
+  end
+
   def place_random_ships
-    spots = @size / 4
-    new_ships = []
-    spots.times do
-      new_ship = [rand(0...@grid.length), rand(0...@grid.length)]
-      until !new_ships.include?(new_ship)
-        new_ship = [rand(0...@grid.length), rand(0...@grid.length)]
-      end
-      new_ships << new_ship
-      self[new_ship] = :S
+    total_ships = @size / 4
+    while self.num_ships < total_ships
+      self[self.get_random_position] = :S
     end
   end
+
+  # def hidden_ships_grid
+  #   new_board = Board.create_grid(@grid.length, @grid.length, :N)
+  #   @grid.each_with_index do |row, x|
+  #     row.each_with_index do |spot, y|
+  #       pos = [x, y]
+  #       new_board[x][y] = self[pos] if self[pos] != :S
+  #     end
+  #   end
+  #   new_board
+  # end
 
   def hidden_ships_grid
-    new_board = Board.create_grid(@grid.length, @grid.length, :N)
-    @grid.each_with_index do |row, x|
-      row.each_with_index do |spot, y|
-        pos = [x, y]
-        new_board[x][y] = self[pos] if self[pos] != :S
+    @grid.map do |row|
+      row.map do |ele|
+        if ele == :S
+          :N
+        else
+          ele
+        end
       end
-    end
-    new_board
-  end
-
-  def self.print_grid(grid)
-    grid.each do |row|
-      puts row.join(" ")
     end
   end
 
