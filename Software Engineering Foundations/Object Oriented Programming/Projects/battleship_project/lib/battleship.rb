5b3535ea866027273416aa361e371c256445de68
erikbenton <erik.benton@hotmail.com>
2019-11-02 08:46:29 -0400

    finishing part 3 of battleship project

--- Software Engineering Foundations/Object Oriented Programming/Projects/battleship_project/lib/battleship.rb
+++ Software Engineering Foundations/Object Oriented Programming/Projects/battleship_project/lib/battleship.rb
@@ -3,4 +3,44 @@ require_relative "player"
 
 class Battleship
 
+  attr_reader :board, :player
+
+  def initialize(n)
+    @player = Player.new
+    @board = Board.new(n)
+    @remaining_misses  = @board.size / 2
+  end
+
+  def start_game
+    @board.place_random_ships
+    puts "There are #{@board.num_ships} ships on the board"
+    @board.print
+  end
+
+  def lose?
+    if @remaining_misses <= 0
+      puts "you lose"
+      return true
+    end
+    false
+  end
+
+  def win?
+    if @board.num_ships == 0
+      puts "you win"
+      return true
+    end
+    false
+  end
+
+  def game_over?
+    return true if self.win? or self.lose?
+    false
+  end
+
+  def turn
+    @remaining_misses -= 1 if !@board.attack(@player.get_move)
+    @board.print
+    puts "Remaining misses: #{@remaining_misses}"
+  end
 end
--- Software Engineering Foundations/Object Oriented Programming/Projects/battleship_project/lib/player.rb
+++ Software Engineering Foundations/Object Oriented Programming/Projects/battleship_project/lib/player.rb
@@ -1,3 +1,6 @@
 class Player
-
+  def get_move
+    puts "enter a position with coordinates separated with a space like `4 7`"
+    input = gets.chomp.split.map { |point| point.to_i }
+  end
 end
