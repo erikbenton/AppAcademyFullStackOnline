e422ac677d4b2198975e5e1b8b4e3c995bbca066
erikbenton <erik.benton@hotmail.com>
2019-11-02 08:29:19 -0400

    finishing part 2 of battleship project

--- Software Engineering Foundations/Object Oriented Programming/Projects/battleship_project/lib/board.rb
+++ Software Engineering Foundations/Object Oriented Programming/Projects/battleship_project/lib/board.rb
@@ -19,16 +19,66 @@ class Board
 
   def [](pos)
     row, col = pos
-    @grid[row][col[1]]
+    @grid[row][col]
   end
 
   def []=(pos, val)
-    @grid[pos[0]][pos[1]] = val
+    row, col = pos
+    @grid[row][col] = val
   end
 
   def num_ships
     flattened_board = @grid.flatten
     flattened_board.count { |ele| ele == :S }
+  end
+
+  def attack(pos)
+    if self[pos] == :S
+      self[pos] = :H
+      puts "you sunk my battleship!"
+      return true
+    else
+      self[pos] = :X
+      return false
+    end
+  end
+
+  def place_random_ships
+    spots = @size / 4
+    new_ships = []
+    spots.times do
+      new_ship = [rand(0...@grid.length), rand(0...@grid.length)]
+      until !new_ships.include?(new_ship)
+        new_ship = [rand(0...@grid.length), rand(0...@grid.length)]
+      end
+      new_ships << new_ship
+      self[new_ship] = :S
+    end
+  end
+
+  def hidden_ships_grid
+    new_board = Board.create_grid(@grid.length, @grid.length, :N)
+    @grid.each_with_index do |row, x|
+      row.each_with_index do |spot, y|
+        pos = [x, y]
+        new_board[x][y] = self[pos] if self[pos] != :S
+      end
+    end
+    new_board
+  end
+
+  def self.print_grid(grid)
+    grid.each do |row|
+      puts row.join(" ")
+    end
+  end
+
+  def cheat
+    Board.print_grid(@grid)
+  end
+
+  def print
+    Board.print_grid(self.hidden_ships_grid)
   end
 
 end
