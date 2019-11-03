c9a406a7b61c2955638ba088b4d3a372e3d82f18
erikbenton <erik.benton@hotmail.com>
2019-11-02 22:23:05 -0400

    finishing the recursion notes

--- Software Engineering Foundations/Recursion/Notes/recursion_notes.rb
+++ Software Engineering Foundations/Recursion/Notes/recursion_notes.rb
@@ -15,5 +15,18 @@ def factorial(num)
   num * factorial(num - 1)
 end
 
+def fibonacci(n)
+  return 0 if n <= 0
+  return 1 if n <= 2
+  fibonacci(n - 1) + fibonacci(n - 2)
+end
+
 count_down(9)
 p factorial(5)
+puts fibonacci(1) # => 1
+puts fibonacci(2) # => 1
+puts fibonacci(3) # => 2
+puts fibonacci(4) # => 3
+puts fibonacci(5) # => 5
+puts fibonacci(6) # => 8
+puts fibonacci(7) # => 13
