b8e604416fc5b5ed00c13f6cfb5cfcfddf365935
Erik Benton <erik.benton@hotmail.com>
2019-11-04 11:59:02 -0500

    making small changes to recap ex 2 after watching solutions

--- Software Engineering Foundations/Recap/Exercises/recap_exercise_2_solution/lib/problems.rb
+++ Software Engineering Foundations/Recap/Exercises/recap_exercise_2_solution/lib/problems.rb
@@ -1,47 +1,54 @@
-# Write a method, least_common_multiple, that takes in two numbers and returns
-# the smallest number that is a mutiple of both of the given numbers
+# Write a method, least_common_multiple, that takes in two numbers and returns the smallest number that is a mutiple
+# of both of the given numbers
 def least_common_multiple(num_1, num_2)
-  ans = num_1
-  while ans % num_1 != 0 or ans % num_2 != 0
-    ans += 1
-  end
-  ans
+    i = 1
+    while i <= num_1 * num_2
+        return i if i % num_1 == 0 && i % num_2 == 0
+        i += 1
+    end
 end
 
 
 # Write a method, most_frequent_bigram, that takes in a string and returns the two adjacent letters that appear the
 # most in the string.
 def most_frequent_bigram(str)
-  bigram_counter = Hash.new { |h, k| h[k] = 0 }
-  (0...(str.length - 1)).each do |idx_1|
-    bigram = str[idx_1...(idx_1 + 2)]
-    bigram_counter[bigram] += 1
-  end
-  bigram_counter.max_by { |k, v| v }[0]
+    freq = Hash.new(0)
+    (0...str.length-1).each do |i|
+        bigram = str[i..i+1]
+        freq[bigram] += 1
+    end
+
+    sorted = freq.sort_by { |str, n| n }
+    sorted.last[0]
 end
+
 
 class Hash
     # Write a method, Hash#inverse, that returns a new hash where the key-value pairs are swapped
     def inverse
-      new_hash = {}
-      self.each { |k, v| new_hash[v] = k }
-      new_hash
+        new_hash = {}
+        self.each do |k, v|
+            new_hash[v] = k
+        end
+        new_hash
     end
 end
 
 
 class Array
-    # Write a method, Array#pair_sum_count, that takes in a target number
-    # returns the number of pairs of elements that sum to the given target
+    # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
     def pair_sum_count(num)
-      count = 0
-      (0...self.length).each do |idx_1|
-        (idx_1...self.length).each do |idx_2|
-          count += 1 if self[idx_1] + self[idx_2] == num
+        count = 0
+        self.each_with_index do |el1, idx1|
+            self.each_with_index do |el2, idx2|
+                if el1 + el2 == num && idx2 > idx1
+                    count += 1
+                end
+            end
         end
-      end
-      count
+        count
     end
+
 
     # Write a method, Array#bubble_sort, that takes in an optional proc argument.
     # When given a proc, the method should sort the array according to the proc.
@@ -57,17 +64,20 @@ class Array
     #
     # This should remind you of the spaceship operator! Convenient :)
     def bubble_sort(&prc)
-      prc ||= Proc.new { |a, b| a <=> b }
-      still_sorting = true
-      while still_sorting
-        still_sorting = false
-        (0...self.length - 1).each do |idx|
-          if prc.call(self[idx], self[idx + 1]) == 1
-            self[idx], self[idx + 1] = self[idx + 1], self[idx]
-            still_sorting = true
-          end
+        prc ||= Proc.new { |a, b| a <=> b }
+
+        sorted = false
+        while !sorted
+            sorted = true
+
+            (0...self.length - 1).each do |i|
+                if prc.call(self[i], self[i + 1]) == 1
+                    self[i], self[i + 1] = self[i + 1], self[i]
+                    sorted = false
+                end
+            end
         end
-      end
-      self
+
+        self
     end
 end
