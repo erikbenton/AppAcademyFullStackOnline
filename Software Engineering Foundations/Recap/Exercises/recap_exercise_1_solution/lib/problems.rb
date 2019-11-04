313b269e68ef857714cd76ce16c298ef3db709f2
Erik Benton <erik.benton@hotmail.com>
2019-11-04 10:06:32 -0500

    making small changes after watching solution and adding debug config for VSCode

--- Software Engineering Foundations/Recap/Exercises/recap_exercise_1_solution/lib/problems.rb
+++ Software Engineering Foundations/Recap/Exercises/recap_exercise_1_solution/lib/problems.rb
@@ -5,33 +5,18 @@
 # Example:
 #
 # all_vowel_pairs(["goat", "action", "tear", "impromptu", "tired", "europe"])   # => ["action europe", "tear impromptu"]
-# def all_vowel_pairs(words)
-#   res = []
-#   vowels = "aeiou".split("")
-#   (0...words.length).each do |idx_1|
-#     word_1 = words[idx_1]
-#     already_seen = word_1.split("").select { |char| vowels.include?(char) }
-#     ((idx_1 + 1)...words.length).each do |idx_2|
-#       word_2 = words[idx_2]
-#       total_seen = already_seen + word_2.split("").select { |char| vowels.include?(char) }
-#       res << "#{word_1} #{word_2}" if vowels.all? { |char| total_seen.include?(char) }
-#     end
-#   end
-#   res
-# end
-
 def all_vowel_pairs(words)
-  res = []
-  vowels = "aeiou".split("")
-  (0...words.length).each do |idx_1|
-    ((idx_1 + 1)...words.length).each do |idx_2|
-      word_1 = words[idx_1]
-      word_2 = words[idx_2]
-      pair = "#{word_1} #{word_2}"
-      res << pair if vowels.all? { |vowel| pair.include?(vowel) }
+    vowels = "aeiou".split("")
+    pairs = []
+
+    (0...words.length).each do |idx1|
+        (idx1 + 1...words.length).each do |idx2|
+            combined = words[idx1] + " " + words[idx2]
+            pairs << combined if vowels.all? { |vowel| combined.include?(vowel) }
+        end
     end
-  end
-  res
+
+    pairs
 end
 
 
@@ -43,7 +28,11 @@ end
 # composite?(9)     # => true
 # composite?(13)    # => false
 def composite?(num)
-  (2...num).any? { |div| num % div == 0 }
+    (2...num).each do |i|
+        return true if num % i == 0
+    end
+
+    false
 end
 
 
@@ -57,9 +46,9 @@ end
 # find_bigrams("the theater is empty", ["cy", "em", "ty", "ea", "oo"])  # => ["em", "ty", "ea"]
 # find_bigrams("to the moon and back", ["ck", "oo", "ha", "at"])        # => ["ck", "oo"]
 def find_bigrams(str, bigrams)
-  bigrams.select { |bigram| str.include?(bigram) }
+    bigrams.select { |bigram| str.include?(bigram) }
 end
-
+ 
 class Hash
     # Write a method, Hash#my_select, that takes in an optional proc argument
     # The method should return a new hash containing the key-value pairs that return
@@ -75,8 +64,13 @@ class Hash
     # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
     # hash_2.my_select                            # => {4=>4}
     def my_select(&prc)
-      prc ||= Proc.new { |k, v| k == v }
-      self.select { |k, v| prc.call(k, v) }
+        prc ||= Proc.new { |k, v| k == v }
+
+        selected = {}
+        self.each do |k, v|
+            selected[k] = v if prc.call(k, v)
+        end
+        selected
     end
 end
 
@@ -90,20 +84,20 @@ class String
     # "cats".substrings     # => ["c", "ca", "cat", "cats", "a", "at", "ats", "t", "ts", "s"]
     # "cats".substrings(2)  # => ["ca", "at", "ts"]
     def substrings(length = nil)
-      res = []
-      (0...(self.length)).each do |idx_1|
-        if length
-          new_str = self[idx_1...(idx_1 + length)]
-          if new_str.length == length
-            res << self[idx_1...(idx_1 + length)]
-          end
-        else
-          ((idx_1)...self.length).each do |idx_2|
-            res << self[idx_1..idx_2]
-          end
+        subs = []
+
+        (0...self.length).each do |start|
+            (start...self.length).each do |ending|
+                sub = self[start..ending]
+                subs << self[start..ending]
+            end
         end
-      end
-      res
+
+        if length.nil?
+            subs
+        else
+            subs.select { |sub| sub.length == length}
+        end
     end
 
 
@@ -117,16 +111,16 @@ class String
     # "bootcamp".caesar_cipher(2) #=> "dqqvecor"
     # "zebra".caesar_cipher(4)    #=> "difve"
     def caesar_cipher(num)
-      alphabet = ("a".."z").to_a
-      res = self.split("").map do |char|
-        if alphabet.include?(char)
-          idx = alphabet.find_index(char)
-          idx = (idx + num) % 26
-          alphabet[idx]
-        else
-          char
+        alphabet = ("a".."z").to_a
+        new_str = ""
+      
+        self.each_char do |char|
+          old_idx = alphabet.index(char)
+          new_idx = old_idx + num
+          new_char = alphabet[new_idx % 26]
+          new_str += new_char
         end
-      end
-      res.join("")
+      
+        return new_str
     end
 end
