f2a116233647c513c8fd2473633d52b171caa0b1
Erik Benton <erik.benton@hotmail.com>
2019-11-17 17:35:36 -0500

    finishing rspec exercise 5, it's messy but it passes

--- Software Engineering Foundations/Additional Projects/rspec_exercise_5/lib/exercise.rb
+++ Software Engineering Foundations/Additional Projects/rspec_exercise_5/lib/exercise.rb
@@ -47,6 +47,7 @@ def zany_zip(*arrs)
   end
   res
 end
+require "byebug"
 
 def maximum(arr, &prc)
   return nil if arr.empty?
@@ -54,7 +55,10 @@ def maximum(arr, &prc)
   res = arr[0]
   arr.each do |ele|
     sub_res = prc.call(ele)
-    res = ele if sub_res >= largest
+    if sub_res >= largest
+      res = ele
+      largest = sub_res
+    end
   end
   res
 end
@@ -69,6 +73,55 @@ end
 
 def max_tie_breaker(arr, prc_1, &prc_2)
   return nil if arr.empty?
-  res = maximum(arr, &prc_2)
-  res
+  max_ele = maximum(arr, &prc_2)
+  max_res = prc_2.call(max_ele)
+  groups = my_group_by(arr, &prc_2)
+  if groups[max_res].length > 1
+    max_ele = maximum(groups[max_res], &prc_1)
+    max_res = prc_1.call(max_ele)
+    groups = my_group_by(arr, &prc_1)
+    if groups[max_res].length > 1
+      return first_satisfy(arr, max_res, prc_1)
+    end
+  end
+  max_ele
+end
+
+def first_satisfy(arr, val, prc)
+  arr.each do |ele|
+    return ele if prc.call(ele) == val
+  end
+end
+
+def silly_syllables(sentence)
+  vowels = "aeiou"
+  num_vowels = sentence.split("").count { |ele| vowels.include?(ele) }
+  if num_vowels < 2
+    return sentence
+  end
+  words = sentence.split.map { |word| silly_words(word) }
+  words.join(" ")
+end
+
+def silly_words(word)
+  vowels = "aeiou"
+  num_vowels = word.split("").count { |ele| vowels.include?(ele) }
+  if num_vowels < 2
+    return word
+  end
+  first = 0
+  last = -1
+  word.split("").each_with_index do |ele, idx|
+    if vowels.include?(ele)
+      first = idx
+      break
+    end
+  end
+  word.split("").reverse.each_with_index do |ele, idx|
+    if vowels.include?(ele)
+      last = word.length - idx
+      break
+    end
+  end
+  word[first...last]
 end
