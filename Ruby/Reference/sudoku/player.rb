5fd4f48b4f3025976fb87b842870affb04fc95f8
Erik Benton <erik.benton@hotmail.com>
2019-11-20 16:22:42 -0500

    adding some helpers to player for getting input

--- Ruby/Reference/sudoku/player.rb
+++ Ruby/Reference/sudoku/player.rb
@@ -3,8 +3,26 @@ class Player
     @name = name
   end
 
-  def get_input
+  def get_guess
     puts "Enter a position and value for the board (e.g. 'x, y, val')"
+    input = self.get_input
+    until self.valid_input?(input)
+      puts "Sorry that was invalid, try again ('x, y, val')"
+      input = self.get_input
+    end
+    input
+  end
+
+  def valid_input?(input)
+    valid = true
+    valid &= input.length == 3
+    valid &= input[0] >= 0
+    valid &= input[1] >= 0
+    valid &= (1..9).to_a.include?(input[2])
+    valid
+  end
+
+  def get_input
     input = gets.chomp
     input = input.split(",").map! { |ele| ele.strip.to_i}
     input
