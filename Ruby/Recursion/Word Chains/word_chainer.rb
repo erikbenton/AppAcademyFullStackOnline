800da485a31c496f55d0162cc0d92b4e09d34f36
Erik Benton <erik.benton@hotmail.com>
2019-11-25 10:06:53 -0500

    finished word_chainer.rb

--- Ruby/Recursion/Word Chains/word_chainer.rb
+++ Ruby/Recursion/Word Chains/word_chainer.rb
@@ -9,7 +9,7 @@ class WordChainer
   def initialize(dictionary_file_name)
     @dictionary = WordChainer.get_dictionary(dictionary_file_name)
     @current_words = []
-    @all_seen_words = []
+    @all_seen_words = Hash.new(nil)
   end
 
   def check_adjacent_words(word, same_length)
@@ -33,12 +33,13 @@ class WordChainer
 
   def run(source, target)
     @current_words = [source]
-    @all_seen_words = [source]
-    until @current_words.empty?# or @current_words.include?(target)
+    @all_seen_words[source] = nil
+    until @current_words.empty? or @current_words.include?(target)
       new_current_words = self.explore_current_words
-      new_current_words.each { |word| puts word }
       @current_words = new_current_words
     end
+    path = self.build_path(target)
+    path.reverse.each { |step| puts step }
   end
 
   def explore_current_words
@@ -46,18 +47,28 @@ class WordChainer
     @current_words.each do |current_word|
       adjacents = self.adjacent_words(current_word)
       adjacents.each do |word|
-        if !@all_seen_words.include?(word)
+        if !@all_seen_words.has_key?(word)
           new_current_words << word
-          @all_seen_words << word
+          @all_seen_words[word] = current_word
         end
       end
     end
     new_current_words
   end
 
+  def build_path(target)
+    path = [target]
+    current_word = target
+    while @all_seen_words[current_word] != nil
+      path << @all_seen_words[current_word]
+      current_word = @all_seen_words[current_word]
+    end
+    path
+  end
+
 end
 
 if __FILE__ == $PROGRAM_NAME
   word_chain = WordChainer.new("./dictionary.txt")
-  word_chain.run("duck", "ruby")
+  word_chain.run("market", "darken")
 end
