a96bf790914fce362db3ec0ef7cf2e7b7f2f21c5
Erik Benton <erik.benton@hotmail.com>
2019-11-27 13:04:25 -0500

    taking notes on ADTs up till Queues

---
+++ Ruby/Data Structures/notes.rb
@@ -0,0 +1,28 @@
+# Data Structures
+#   Array
+#   Hash
+#   Class
+
+# Abstract Data Types
+#   Set
+#     - include?
+#     - <<
+#     - delete
+# Can be implemented by an Array [3, "Hi"]
+# Would say that the Set is implemented by an Array
+# Can also use Hash { 3 => true, "hello" => true }
+#   Map/Dictionary
+#     - set
+#     - get
+#     - delete
+
+# How the ADT is implemented matters, but what is most important is that
+# it fulfills its requirements
+
+# Stack
+# LIFO - Last In, First Out
+# Usually implemented by an Array
+
+# Queue
+# FIFO - First In, First Out
+# Enqueue and dequeue
