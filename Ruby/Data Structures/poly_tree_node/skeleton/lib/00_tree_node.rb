8bf85a8879bd13cb07d66e2275fc67ff6d546535
Erik Benton <erik.benton@hotmail.com>
2019-12-03 10:12:42 -0500

    adding #parent= method

--- Ruby/Data Structures/poly_tree_node/skeleton/lib/00_tree_node.rb
+++ Ruby/Data Structures/poly_tree_node/skeleton/lib/00_tree_node.rb
@@ -1,8 +1,23 @@
 class PolyTreeNode
-  attr_reader :value, :parent, :children
+  attr_reader :value, :children
+
   def initialize(value)
     @value = value
     @parent = nil
     @children = []
+  end
+
+  def parent=(parent_node)
+    @parent = parent_node
+    return unless parent_node
+    parent_node.children << self
+  end
+
+  def parent
+    @parent
+  end
+
+  def add_child(new_node)
+    new_node.parent = new_node
   end
 end
