ebf48ee3f029369b80ddc5ad279f8572abf06e4b
Erik Benton <erik.benton@hotmail.com>
2019-12-03 11:02:53 -0500

    completing depth first search

--- Ruby/Data Structures/poly_tree_node/skeleton/lib/00_tree_node.rb
+++ Ruby/Data Structures/poly_tree_node/skeleton/lib/00_tree_node.rb
@@ -26,6 +26,16 @@ class PolyTreeNode
     child.parent = nil
     raise "Not a child" unless @children.delete(child)
   end
+
+  def dfs(target)
+    return nil if self.nil?
+    return self if self.value == target
+    @children.each do |child|
+      search_res = child.dfs(target)
+      return search_res unless search_res.nil?
+    end
+    nil
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
