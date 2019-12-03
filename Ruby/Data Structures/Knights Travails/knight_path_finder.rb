6ba6625f10f827e188408605ff3ba43988d6ab87
Erik Benton <erik.benton@hotmail.com>
2019-12-03 11:41:02 -0500

    adding initialization method and PolyTreeNode class

--- Ruby/Data Structures/Knights Travails/knight_path_finder.rb
+++ Ruby/Data Structures/Knights Travails/knight_path_finder.rb
@@ -1,3 +1,18 @@
+require_relative "poly_tree_node"
+
 class KnightPathFinder
-  
+  def self.valid_moves(pos)
+    moves = []
+    changes = [[1,2],[-1,-2],[-1,2],[1,-2],[2,1],[-2,-1],[-2,1],[2,-1]]
+    changes.each do |change|
+      move = [pos[0] + change[0], pos[1] + change[1]]
+      moves << move if move[0].between?(0,8) and move[1].between?(0,8)
+    end
+    moves
+  end
+
+  def initialize(root)
+    @root_node = PolyTreeNode.new(root)
+    @considered_positions = [root]
+  end
 end
---
+++ Ruby/Data Structures/Knights Travails/poly_tree_node.rb
@@ -0,0 +1,50 @@
+require "byebug"
+class PolyTreeNode
+  attr_reader :value, :children
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(new_parent)
+    @parent.children.delete(self) unless @parent.nil?
+    @parent = new_parent
+    new_parent.children << self unless new_parent.nil?
+  end
+
+  def parent
+    @parent
+  end
+
+  def add_child(child)
+    child.parent = self
+  end
+
+  def remove_child(child)
+    child.parent = nil
+    raise "Not a child" unless @children.delete(child)
+  end
+
+  def dfs(target)
+    return nil if self.nil?
+    return self if self.value == target
+    @children.each do |child|
+      search_res = child.dfs(target)
+      return search_res unless search_res.nil?
+    end
+    nil
+  end
+
+  def bfs(target)
+    queue = [self]
+    until queue.empty?
+      el = queue.shift
+      return el if el.value == target
+      el.children.each do |child|
+        queue << child
+      end
+    end
+  end
+end
