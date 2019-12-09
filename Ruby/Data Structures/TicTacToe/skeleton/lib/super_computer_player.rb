360fadabea0e9867b35906ce71cbbc003ac24591
Erik Benton <erik.benton@hotmail.com>
2019-12-09 11:24:11 -0500

    finishing TicTacToe but not sure if it's really done how they want it to be done...

--- Ruby/Data Structures/TicTacToe/skeleton/lib/super_computer_player.rb
+++ Ruby/Data Structures/TicTacToe/skeleton/lib/super_computer_player.rb
@@ -1,7 +1,16 @@
 require_relative 'tic_tac_toe_node'
-
+require "byebug"
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    new_node = TicTacToeNode.new(game.board, mark)
+    new_mark = (mark == :x) ? :o : :x
+    contingency = nil
+    new_node.children.each do |child|
+      return child.prev_move_pos if child.winning_node?(mark)
+      contingency = child if child.losing_node?(new_mark)
+    end
+    raise TicTacToe::IllegalMoveError if contingency.nil?
+    contingency.prev_move_pos
   end
 end
 
