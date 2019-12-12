a97e09dc793c1cb176f2b1151e46d1aa2b1a579d
Erik Benton <erik.benton@hotmail.com>
2019-12-12 16:28:37 -0500

    finishing specs for board class

--- Ruby/OOP/mancala/mancala_project/skeleton/lib/board.rb
+++ Ruby/OOP/mancala/mancala_project/skeleton/lib/board.rb
@@ -25,21 +25,40 @@ class Board
   def make_move(start_pos, current_player_name)
     wallet = @cups[start_pos].length
     @cups[start_pos] = []
-    current_pos = update_position(start_pos, current_player_name)
+    current_pos = start_pos
     until wallet == 0
+      current_pos = update_position(current_pos, current_player_name)
       if valid_cup?(current_pos, current_player_name)
         @cups[current_pos].push(:stone)
         wallet -= 1
       end
-      current_pos = update_position(current_pos, current_player_name)
     end
     render
+    next_turn(current_pos)
+    if ending_cup?(current_pos)
+      return :switch
+    end
+    if players_cup?(current_pos, current_player_name)
+      return :prompt
+    end
+    return current_pos
   end
 
   def valid_cup?(cup_pos, player_name)
     return false if cup_pos == 6 && player_name != @name1
     return false if cup_pos == 13 && player_name != @name2
     true
+  end
+
+  def players_cup?(pos, player_name)
+    return true if pos == 6 && player_name == @name1
+    return true if pos == 13 && player_name == @name2
+    false
+  end
+
+  def ending_cup?(pos)
+    return true if @cups[pos].length == 1 && pos != 6 && pos != 13
+    false
   end
 
   def update_position(pos, current_player_name)
@@ -64,8 +83,18 @@ class Board
   end
 
   def one_side_empty?
+    bottom_cups = []
+    top_cups = []
+    (0..5).to_a.each { |idx| bottom_cups << @cups[idx] }
+    (7..12).to_a.each { |idx| top_cups << @cups[idx] }
+    return true if bottom_cups.all? { |cup| cup.empty? }
+    return true if top_cups.all? { |cup| cup.empty? }
+    false
   end
 
   def winner
+    return :draw if @cups[6].length == @cups[13].length
+    champ = @cups[6].length > @cups[13].length ? @name1 : @name2
+    champ
   end
 end
