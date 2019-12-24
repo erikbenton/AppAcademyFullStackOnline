5450fab32d909a69d15bb64e6557ae143a3e060a
Erik Benton <erik.benton@hotmail.com>
2019-12-24 15:09:55 -0500

    moving Board#render to Display#render

--- Ruby/OOP/chess_project/display.rb
+++ Ruby/OOP/chess_project/display.rb
@@ -1,12 +1,25 @@
 require "colorize"
 require_relative "cursor.rb"
+require_relative "board.rb"
 class Display
+  attr_accessor :board
+
   def initialize(board)
     @board = board
     @cursor = Cursor.new([0,0], board)
   end
 
   def render
-    board.render
+    board.rows.each do |row|
+      row.each do |piece|
+        print piece.symbol + " "
+      end
+      puts
+    end
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  disp = Display.new(Board.new)
+  disp.render
 end
