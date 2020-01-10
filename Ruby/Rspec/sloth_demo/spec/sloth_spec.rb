45721637aab79b1c0f36d0cefe3d8810501522a6
Erik Benton <erik.benton@hotmail.com>
2020-01-10 13:38:39 -0500

    writing test for initialize with empty foods array

--- Ruby/Rspec/sloth_demo/spec/sloth_spec.rb
+++ Ruby/Rspec/sloth_demo/spec/sloth_spec.rb
@@ -1,12 +1,15 @@
 require "sloth"
 
 describe Sloth do
-  
   subject(:sloth) { Sloth.new("Herbert") }
 
   describe "#initialize" do
     it "assigns a name" do
       expect(sloth.name).to eq("Herbert")
+    end
+
+    it "starts with an empty foods array" do
+      expect(sloth.foods).to be_empty
     end
   end
 end
