444acd990bfe7ab62eb336cabefe8339e8e2af8a
Erik Benton <erik.benton@hotmail.com>
2020-01-10 13:34:12 -0500

    Sloth#initialize now passes

---
+++ Ruby/Rspec/sloth_demo/lib/sloth.rb
@@ -0,0 +1,6 @@
+class Sloth
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
--- Ruby/Rspec/sloth_demo/spec/sloth_spec.rb
+++ Ruby/Rspec/sloth_demo/spec/sloth_spec.rb
@@ -1,6 +1,9 @@
 require "sloth"
 
 describe Sloth do
+  
+  subject(:sloth) { Sloth.new("Herbert") }
+
   describe "#initialize" do
     it "assigns a name" do
       expect(sloth.name).to eq("Herbert")
