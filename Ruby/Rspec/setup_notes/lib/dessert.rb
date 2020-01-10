e80d6711a4ad33728a6aba5e061f1be0567b1ebe
Erik Benton <erik.benton@hotmail.com>
2020-01-10 12:17:57 -0500

    passes new test to verify amount is a number

--- Ruby/Rspec/setup_notes/lib/dessert.rb
+++ Ruby/Rspec/setup_notes/lib/dessert.rb
@@ -2,6 +2,7 @@ class Dessert
   attr_reader :type, :amount
 
   def initialize(type, amount)
+    raise "amount must be a number" unless amount.is_a?(Numeric)
     @amount = amount
     @type = amount > 100 ? "giant #{type}" : type
   end
