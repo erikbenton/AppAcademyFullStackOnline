594931cb6a6e1f5a551bc54bbe5cfe1868e5fd54
Erik Benton <erik.benton@hotmail.com>
2020-01-13 10:11:43 -0500

    working on dessert homework, just started

---
+++ Ruby/Rspec/dessert_homework/skeleton/spec/dessert_spec.rb
@@ -0,0 +1,48 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef") }
+  
+  describe "#initialize" do
+    subject(:dessert) { Dessert.new("pie", 5, chef) }
+
+    it "sets a type" do
+      expect(dessert.type).to eq("pie")
+    end
+
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(5)
+    end
+
+    it "starts ingredients as an empty array"
+
+    it "raises an argument error when given a non-integer quantity"
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array"
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array"
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity"
+
+    it "raises an error if the amount is greater than the quantity"
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name"
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in"
+  end
+end
