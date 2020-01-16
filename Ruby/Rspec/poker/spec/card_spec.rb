949dd59788eaf675df052393602e10fa0b86e5de
Erik Benton <erik.benton@hotmail.com>
2020-01-16 08:58:08 -0500

    starting tdd poker exercise

---
+++ Ruby/Rspec/poker/spec/card_spec.rb
@@ -0,0 +1,14 @@
+require "card"
+
+describe Card do
+  describe "#initialize" do
+    card = Card.new(2, :diamonds)
+    it "has a value" do
+      expect(card.value).to eq(2)
+    end
+
+    it "has a suit" do
+      expect(card.suit).to eq(:diamonds)
+    end
+  end
+end
