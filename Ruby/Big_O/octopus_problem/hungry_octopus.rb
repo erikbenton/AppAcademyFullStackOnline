d2fc905e76cb27c56fe6820a668e74f127eb7196
Erik Benton <erik.benton@hotmail.com>
2020-01-21 16:28:06 -0500

    finishing the work day

--- Ruby/Big_O/octopus_problem/hungry_octopus.rb
+++ Ruby/Big_O/octopus_problem/hungry_octopus.rb
@@ -1,7 +1,8 @@
+require "byebug"
 # Use bubble sort and get the last one
 def sluggish_octopus(fish)
   sorted = false
-  while !sorted
+  until sorted
     sorted = true
     first = 0
     second = 1
@@ -17,6 +18,40 @@ def sluggish_octopus(fish)
   fish[-1]
 end
 
-fishies = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh'];
+fishies = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh', "fssshhh"];
 
-puts sluggish_octopus(fishies)
+puts sluggish_octopus(fishies.dup)
+
+# Merge sort
+def dominant_octopus(fish)
+  return fish if fish.length < 2
+
+  # Find the middle
+  mid = fish.length / 2
+
+  # Call merge sort on both sides of the midpoint
+  left= fish[0...mid]
+  right = fish[mid..-1]
+
+  sorted_left, sorted_right = dominant_octopus(left), dominant_octopus(right)
+  merge(left, right)
+end
+
+def merge(left, right)
+  sorted_arr = []
+  until left.empty? || right.empty?
+    if left.first.length < right.first.length
+      sorted_arr << left.shift
+    else
+      sorted_arr << right.shift
+    end
+  end
+  left + sorted_arr + right
+end
+
+puts 
+puts fishies
+puts
+puts dominant_octopus(fishies)
+puts
+puts fishies[-1]
