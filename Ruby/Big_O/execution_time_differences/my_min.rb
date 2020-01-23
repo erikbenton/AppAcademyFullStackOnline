0fd0bb54cf6ea89417ca499162203ef1de31c533
Erik Benton <erik.benton@hotmail.com>
2020-01-23 15:26:08 -0500

    finishing phase 2 of my_min

--- Ruby/Big_O/execution_time_differences/my_min.rb
+++ Ruby/Big_O/execution_time_differences/my_min.rb
@@ -10,3 +10,18 @@ puts my_min(list)  # =>  -10
 # This has O(n^2) time complexity since it is a loop
 # in another loop, so at worst it'll have to do:
 # n inner loops for n outer loops, so n * n => O(n^2)
+
+def my_min_lin(arr)
+  min = arr.first
+  arr.each do |ele|
+    min = ele if ele < min
+  end
+  min
+end
+
+puts
+puts my_min_lin(list)  # =>  -10
+
+# This is O(n) time complexity since all n elements
+# Need to be looked at/compared each time
+# So n steps => O(n)
