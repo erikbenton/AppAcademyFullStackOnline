34770c5eaf196af1c1dc3f2d0feec0f0dcad6b95
Erik Benton <erik.benton@hotmail.com>
2020-01-24 11:25:26 -0500

    writing code for okay_two_sum?

--- Ruby/Big_O/two_sum/two_sum.rb
+++ Ruby/Big_O/two_sum/two_sum.rb
@@ -1,3 +1,4 @@
+require "byebug"
 def bad_two_sum?(arr, target)
   arr.each_with_index do |num, idx_1|
     ((idx_1+1)...arr.length).to_a.each do |idx_2|
@@ -10,3 +11,20 @@ end
 arr = [0, 1, 5, 7]
 puts bad_two_sum?(arr, 6) # => should be true
 puts bad_two_sum?(arr, 10) # => should be false
+
+# This would have a time complexity of O(n^2) because (at worst) it has 2 loops
+# that will need to go n * (n + 1) / 2 steps since the outer loop will have to
+# go to the second to the end (n - 1) and 
+
+def okay_two_sum?(arr, target)
+  sorted_arr = arr.sort
+  arr.each_with_index do |num, idx|
+    # debugger
+    res = arr.bsearch_index { |ele| ele <=> target - num }
+    return true if res != nil && idx != res
+  end
+  false
+end
+
+puts okay_two_sum?(arr, 6) # => should be true
+puts okay_two_sum?(arr, 10) # => should be false
