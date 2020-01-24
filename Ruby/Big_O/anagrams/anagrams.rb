aeac83f1993cd367631d055b5c83fbfc41093531
Erik Benton <erik.benton@hotmail.com>
2020-01-24 09:13:44 -0500

    wrote code for second_anagram?

--- Ruby/Big_O/anagrams/anagrams.rb
+++ Ruby/Big_O/anagrams/anagrams.rb
@@ -6,3 +6,20 @@ end
 
 puts first_anagram?("gizmo", "sally")
 puts first_anagram?("lives", "elvis")
+# puts first_anagram?("godzaailla", "aagdozilla")
+
+def second_anagram?(first_word, second_word)
+  return false if first_word.length != second_word.length
+  first = first_word.split("")
+  second = second_word.split("")
+  first.each do |char|
+    res = second.find_index(char)
+    second[res] = " " unless res.nil?
+  end
+  second.all? { |char| char == " "}
+end
+
+puts
+puts second_anagram?("gizmo", "sally")
+puts second_anagram?("lives", "elvis")
+# puts second_anagram?("godzillaaa", "gdozillaaa")
