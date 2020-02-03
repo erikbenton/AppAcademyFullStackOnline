1ab3da2f05674d50045c24b4c94e2e973eeb5820
Erik Benton <erik.benton@hotmail.com>
2020-02-03 08:48:03 -0500

    finished 'Hashing' video

--- Ruby/LRU/notes.rb
+++ Ruby/LRU/notes.rb
@@ -3,3 +3,24 @@
 # Resizing Int Set
   # Approaching O(1)
 # How a Hash is formed
+
+# ========
+#  Hashes
+# ========
+
+# Deterministic
+# Uniform 
+# Highly-sensitive
+# Ideally non reverse-engineerable
+
+# Types of functions
+  # Fast hashing fns
+    # Cityhash
+    # CRC32
+    # Mumurhash
+  # Cryptographic
+    # MD5 - Bad
+    # SHA2
+    # Blowfish
+
+# Every object in Ruby has a Object#hash method to be "hashable"
