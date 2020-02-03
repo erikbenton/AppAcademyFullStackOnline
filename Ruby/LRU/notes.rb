ddecfd8b2ff114e11ff69e9cb1d983337522c165
Erik Benton <erik.benton@hotmail.com>
2020-02-03 11:56:49 -0500

    finishing watching video on LL and Hashmaps

--- Ruby/LRU/notes.rb
+++ Ruby/LRU/notes.rb
@@ -54,3 +54,45 @@
 # "hello".hash % 8 -> 1
 # "dolly".hash % 8 -> 6
 # [ [], [2, "hello"], [16], [], [], [], [4, "dolly"], [8] ]
+
+# ==============
+#  Linked Lists
+# ==============
+
+# Singly-Linked-List
+  # Head
+  # Tail
+  # Nodes
+    # Parent
+    # Child/Next
+    # Value
+
+# Find -> O(n)
+# Push -> O(n) -> O(1)
+# Unshifting -> O(1)
+
+# Doubly-Linked_List
+  # prev
+
+# ==========
+#  Hash Map
+# ==========
+
+# { 1 => 1, 2 => 4, 3 => 9, 5 => 25 }
+#  [ 
+  # LL
+  # LL
+  # LL
+  # LL 
+# ]
+
+# 1.hash % 4 = 2
+# 2.hash % 4 = 0
+# 3.hash % 4 = 1
+# 5.hash % 4 = 1
+#  [ 
+  # LL -> [2,4]
+  # LL -> [3,9] -> [5,25]
+  # LL -> [1,1]
+  # LL
+# ]
