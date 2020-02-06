40dbcf8d72da18a288b7f3a9423d625c6b540067
Erik Benton <erik.benton@hotmail.com>
2020-02-06 16:22:00 -0500

    finishing code for passing HashSet specs

--- Ruby/LRU/LRU_cache_project/skeleton/lib/p02_hashing.rb
+++ Ruby/LRU/LRU_cache_project/skeleton/lib/p02_hashing.rb
@@ -3,30 +3,30 @@ class Integer
 end
 
 class Array
-  def hash
-    num = 0
-    self.each do |ele|
-      num ^= (num + ele)
-    end
-    num
-  end
+  # def hash
+  #   num = 0
+  #   self.each do |ele|
+  #     num ^= (num + ele)
+  #   end
+  #   num
+  # end
 end
 
 class String
-  def hash
-    letters = self.split("").map { |char| char.ord }
-    letters.hash
-  end
+  # def hash
+  #   letters = self.split("").map { |char| char.ord }
+  #   letters.hash
+  # end
 end
 
 class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
-  def hash
-    num = 0
-    self.each_pair do |k, v|
-      num ^= k.hash + v.hash
-    end
-    num
-  end
+  # def hash
+  #   num = 0
+  #   self.each_pair do |k, v|
+  #     num ^= (k.hash + v.hash)
+  #   end
+  #   num
+  # end
 end
--- Ruby/LRU/LRU_cache_project/skeleton/lib/p03_hash_set.rb
+++ Ruby/LRU/LRU_cache_project/skeleton/lib/p03_hash_set.rb
@@ -7,18 +7,33 @@ class HashSet
   end
 
   def insert(key)
+    resize! if @count >= num_buckets
+    return false if include?(key)
+    bucket = self[key.hash]
+    bucket << key
+    @count += 1
+    true
   end
 
   def include?(key)
+    bucket = self[key.hash]
+    bucket.include?(key)
   end
 
   def remove(key)
+    return unless include?(key)
+    bucket = self[key.hash]
+    bucket.delete(key)
+    @count -= 1
+    @count = 0 if @count < 0
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    num %= num_buckets
+    @store[num]
   end
 
   def num_buckets
@@ -26,5 +41,11 @@ class HashSet
   end
 
   def resize!
+    temp_store = @store.dup
+    @store = Array.new(num_buckets * 2) { Array.new }
+    temp_store.flatten.each do |num|
+      bucket = self[num]
+      bucket << num
+    end
   end
 end
