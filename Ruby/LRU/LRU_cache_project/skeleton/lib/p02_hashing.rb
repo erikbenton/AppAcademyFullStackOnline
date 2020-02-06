f9fa591f499dc17ed51fb909075b80efe514ce08
Erik Benton <erik.benton@hotmail.com>
2020-02-06 15:55:56 -0500

    writing hash methods that pass specs

--- Ruby/LRU/LRU_cache_project/skeleton/lib/p02_hashing.rb
+++ Ruby/LRU/LRU_cache_project/skeleton/lib/p02_hashing.rb
@@ -4,11 +4,18 @@ end
 
 class Array
   def hash
+    num = 0
+    self.each do |ele|
+      num ^= (num + ele)
+    end
+    num
   end
 end
 
 class String
   def hash
+    letters = self.split("").map { |char| char.ord }
+    letters.hash
   end
 end
 
@@ -16,6 +23,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    num = 0
+    self.each_pair do |k, v|
+      num ^= k.hash + v.hash
+    end
+    num
   end
 end
