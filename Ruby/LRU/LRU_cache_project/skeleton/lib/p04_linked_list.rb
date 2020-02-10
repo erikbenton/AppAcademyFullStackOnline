b4a308c3f7fbe7164749a269f0203d77462c1f7b
Erik Benton <erik.benton@hotmail.com>
2020-02-10 08:58:26 -0500

    writing most of the LinkedList phase

--- Ruby/LRU/LRU_cache_project/skeleton/lib/p04_linked_list.rb
+++ Ruby/LRU/LRU_cache_project/skeleton/lib/p04_linked_list.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class Node
   attr_reader :key
   attr_accessor :val, :next, :prev
@@ -14,15 +15,23 @@ class Node
   end
 
   def remove
-    # optional but useful, connects previous link to next link
-    # and removes self from list.
+    prev_node = self.prev
+    next_node = self.next
+    prev_node.next = next_node
+    next_node.prev = prev_node
+    self.next = nil
+    self.prev = nil
   end
 end
 
 class LinkedList
   include Enumerable
-  
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -31,27 +40,59 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    current_node = first
+    until current_node.next.nil?
+      return current_node.val if current_node.key == key
+      current_node = current_node.next
+    end
+    nil
   end
 
   def include?(key)
+    current_node = first
+    until current_node.next.nil?
+      return true if current_node.key == key
+      current_node = current_node.next
+    end
+    false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    last.next = new_node
+    new_node.prev = last
+    new_node.next = @tail
+    @tail.prev = new_node
   end
 
   def update(key, val)
+    current_node = first
+    until current_node.next.nil?
+      return current_node.val = val if current_node.key == key
+      current_node = current_node.next
+    end
+    nil
   end
 
   def remove(key)
+    current_node = first
+    until current_node.next.nil?
+      return current_node.remove if current_node.key == key
+      current_node = current_node.next
+    end
+    nil
   end
 
   def each
