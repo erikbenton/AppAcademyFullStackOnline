8d14d8cd375821f4e80dc24308226c0e4ae5069f
Erik Benton <erik.benton@hotmail.com>
2020-02-24 14:32:27 -0500

    had to make modifications to sqlzoo.rb for windows pg user

--- SQL/SQLZoo/skeleton/lib/sqlzoo.rb
+++ SQL/SQLZoo/skeleton/lib/sqlzoo.rb
@@ -1,7 +1,7 @@
 require 'pg'
 
 def execute(sql)
-  conn = PG::Connection.open(:dbname => 'sqlzoo')
+  conn = PG::Connection.open(:dbname => 'sqlzoo', :user => 'postgres', :password => 'Sprite1123')
   query_result = conn.exec(sql).values
   conn.close
 
