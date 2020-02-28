296e7951b8b5f71b8687571dfe39b8c9944f51fa
Erik Benton <erik.benton@hotmail.com>
2020-02-28 12:00:16 -0500

    adding find_by_title to Plays database ORM

--- SQL/Building a Database/plays.rb
+++ SQL/Building a Database/plays.rb
@@ -22,6 +22,18 @@ class Play
     data.map { |datum| Play.new(datum) }
   end
 
+  def self.find_by_title(title)
+    play = PlayDBConnection.instance.execute(<<-SQL, title)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        plays.title = ?
+    SQL
+    play
+  end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
