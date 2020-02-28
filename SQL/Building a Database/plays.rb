9bcc5c0bc3f64ee401618cb0b588104683f902a8
Erik Benton <erik.benton@hotmail.com>
2020-02-28 09:51:52 -0500

    creating update method for play data

--- SQL/Building a Database/plays.rb
+++ SQL/Building a Database/plays.rb
@@ -13,15 +13,31 @@ end
 
 class Play
   def self.all
-    
+    data = PlayDBConnection.instance.execute("
+      SELECT
+        *
+      FROM
+        plays")
+    data.map { |datum| Play.new(datum) }
   end
 
   def initialize(options)
-    
+    @id = options['id']
+    @title = options['title']
+    @year = options['year']
+    @playwright_id = options['playwright_id']
   end
 
   def create
-    
+    raise "#{self} is already in the database" unless @id.nil?
+    #heredoc - Prevents SQL injection attacks
+    PlayDBConnection.instance.execute(<<-SQL, @title, @year, @playwright_id)
+      INSERT INTO
+        plays (title, year, playwright_id)
+      VALUES
+        (?, ?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
   end
 
   def update
