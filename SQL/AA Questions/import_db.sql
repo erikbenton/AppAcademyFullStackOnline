2d68881277a861c8ee4f46fdf567433b547b8209
Erik Benton <erik.benton@hotmail.com>
2020-03-02 10:12:47 -0500

    workin on the beginning of the ORM

--- SQL/AA Questions/import_db.sql
+++ SQL/AA Questions/import_db.sql
@@ -1,5 +1,12 @@
 PRAGMA foreign_keys = ON;
 
+-- Dropping all the tables first
+DROP TABLE questions_likes;
+DROP TABLE questions_follows;
+DROP TABLE replies;
+DROP TABLE questions;
+DROP TABLE users;
+
 -- Creating A Users table
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
@@ -14,13 +21,14 @@ CREATE TABLE questions (
   body TEXT NOT NULL,
   author_id INTEGER NOT NULL,
 
-  FOREIGN KEY (author_id) REFERENCES questions(id)
+  FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
 -- Creating a Question Follows table
 CREATE TABLE questions_follows (
-  question_id INTEGER,
-  follower_id INTEGER,
+  id INTEGER PRIMARY KEY,
+  question_id INTEGER NOT NULL,
+  follower_id INTEGER NOT NULL,
 
   FOREIGN KEY (question_id) REFERENCES questions(id),
   FOREIGN KEY (follower_id) REFERENCES users(id)
@@ -41,9 +49,67 @@ CREATE TABLE replies (
 
 -- Create a Question Likes table
 CREATE TABLE questions_likes (
-  user_id INTEGER,
-  question_id INTEGER,
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
 
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (question_id) REFERENCES questions(id)
 );
+
+-- Put in some Users
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ('Erik', 'Benton'),
+  ('Utas', 'Nenaksieh'),
+  ('Danny', 'D''evito');
+
+-- Put in some questions
+INSERT INTO
+  questions (title, body, author_id)
+VALUES
+  ('Can anyone tell ma about JOINs?',
+  'SQL is silly and I don''t know how to JOIN',
+    (
+      SELECT
+        id
+      FROM
+        users
+      WHERE
+      users.fname LIKE '%Erik%'
+    )
+  ),
+  ('Need help with...',
+  'My car won''t start',
+    (
+      SELECT
+        id
+      FROM
+        users
+      WHERE
+      users.fname LIKE '%Utas%'
+    )
+  ),
+  ('Who knows how to...',
+  'Change my car''s oil',
+    (
+      SELECT
+        id
+      FROM
+        users
+      WHERE
+      users.lname LIKE '%Benton%'
+    )
+  ),
+  ('Why can''t I figure this out?',
+  'Any help with the math here would be great...',
+    (
+      SELECT
+        id
+      FROM
+        users
+      WHERE
+      users.lname LIKE '%D''evito%'
+    )
+  );
---
+++ SQL/AA Questions/user.rb
@@ -0,0 +1,40 @@
+require_relative 'questions_database.rb'
+require 'byebug'
+
+class User
+  attr_accessor :id, :fname, :lname
+
+  def self.all
+    users = QuestionsDBConnection.instance.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        users;
+    SQL
+    users.map { |user| User.new(user) }
+  end
+
+  def self.find_by_id(id)
+    user = QuestionsDBConnection.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        users.id = ?;
+    SQL
+    User.new(user.first)
+  end
+  
+  def initialize(options)
+    @fname = options['fname']
+    @id = options['id']
+    @lname = options['lname']
+  end
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+  p User.all
+  p User.find_by_id(1)
+end
