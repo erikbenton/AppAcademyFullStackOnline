25d00418bf1564c93a34d328f0203222c0dce060
Erik Benton <erik.benton@hotmail.com>
2020-03-05 16:21:48 -0500

    adding simple associations for Enrollment to Join User - Course

--- SQL/Active Record/associations_exercise/app/models/course.rb
+++ SQL/Active Record/associations_exercise/app/models/course.rb
@@ -1,2 +1,6 @@
 class Course < ApplicationRecord
+  has_many :students,
+    class_name: :User,
+    foreign_key: :student_id,
+    primary_key: :id
 end
--- SQL/Active Record/associations_exercise/app/models/user.rb
+++ SQL/Active Record/associations_exercise/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :courses,
+    class_name: :Course,
+    foreign_key: :course_id,
+    primary_key: :id
 end
