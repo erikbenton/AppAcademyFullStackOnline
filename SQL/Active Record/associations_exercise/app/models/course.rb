003debc89df55836e938e8ebc8129ad9182cc8b6
Erik Benton <erik.benton@hotmail.com>
2020-03-06 08:04:02 -0500

    adding associations for the User and Course classes #enrollments

--- SQL/Active Record/associations_exercise/app/models/course.rb
+++ SQL/Active Record/associations_exercise/app/models/course.rb
@@ -1,6 +1,11 @@
 class Course < ApplicationRecord
-  has_many :students,
+  has_many :enrolled_students,
     class_name: :User,
     foreign_key: :student_id,
+    primary_key: :id
+
+  has_many :enrollments,
+    class_name: :Enrollment,
+    foreign_key: :course_id,
     primary_key: :id
 end
--- SQL/Active Record/associations_exercise/app/models/user.rb
+++ SQL/Active Record/associations_exercise/app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  has_many :courses,
+  has_many :enrolled_courses,
     class_name: :Course,
     foreign_key: :course_id,
+    primary_key: :id
+
+  has_many :enrollments,
+    class_name: :Enrollment,
+    foreign_key: :student_id,
     primary_key: :id
 end
