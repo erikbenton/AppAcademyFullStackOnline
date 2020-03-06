26741b899f1d1e40ba05b950dc8cb80b3b3b40fb
Erik Benton <erik.benton@hotmail.com>
2020-03-06 16:19:32 -0500

    just refactoring NBAPlayer demo a little

--- Javascript/JavaScript Fundamentals/nbaPlayer.js
+++ Javascript/JavaScript Fundamentals/nbaPlayer.js
@@ -1,15 +1,20 @@
-function NBAPlayer(name, team, position)
-{
-  this.name = name
-  this.team = team
-  this.position = position
-
-  this.dunk = function()
-  {
+class NBAPlayer {
+  constructor(name, team, position) {
+    this.name = name
+    this.team = team
+    this.position = position
+  }
+  dunk() {
     console.log(this.name + " has just dunked")
+  }
+  say_my_name() {
+    console.log(this.name)
   }
 }
 
+
 let mike = new NBAPlayer("Mike", "Bulls", "center")
+let curry = new NBAPlayer("Steph Curry", "GS Warriors", "Point Guard");
 
 mike.dunk()
+curry.say_my_name()
