aa00a6588d6b9c7c97b0c5fa75bc041ee84c44f8
Erik Benton <erik.benton@hotmail.com>
2020-03-09 09:55:01 -0400

    finishing notes on closures_callbacks

--- Javascript/JavaScript Fundamentals/closures_callbacks.js
+++ Javascript/JavaScript Fundamentals/closures_callbacks.js
@@ -27,7 +27,22 @@ function multiplication(arr) {
   return sum;
 }
 
+let callback = function() {
+  console.log("It has been 5 seconds");
+};
 
 
 soundMaker("yee-haw", 5);
 console.log(multiplication([1,2,3,4]));
+
+let timeToWait = 5000;
+// global.setTimeout(callback, timeToWait);
+
+// Or, more likely to see
+// global.setTimeout(function() {
+//     console.log("It has been 5 seconds");
+// }, timeToWait);
+// ^^ this is ES5
+
+// This is ES6 (preferred syntax)
+global.setTimeout(() => console.log("It has been 5 seconds!"), timeToWait);
