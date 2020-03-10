d752f6131813f3e865d1eddacaab0f949c6b7b18
erikbenton <erik.benton@hotmail.com>
2020-03-10 07:54:51 -0400

    starting js fundamentals homework

---
+++ Javascript/JavaScript Fundamentals/javascript_fundamentals/javascript_fundamentals_part_1.js
@@ -0,0 +1,13 @@
+const madlib = function(verb, adjective, noun) {
+  verb = verb.toUpperCase();
+  adjective = adjective.toUpperCase();
+  noun = noun.toUpperCase();
+  console.log(`We shall ${verb} the ${adjective} ${noun}`);
+  return;
+};
+
+madlib('make', 'best', 'guac');
+
+const isSubString = function(searchString, subString) {
+  
+};
