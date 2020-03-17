c072e1a74f076f9716bd3160719351c683c413ef
erikbenton <erik.benton@hotmail.com>
2020-03-17 16:35:56 -0400

    finished js fundamentals part 1

--- Javascript/JavaScript Fundamentals/javascript_fundamentals/javascript_fundamentals_part_1.js
+++ Javascript/JavaScript Fundamentals/javascript_fundamentals/javascript_fundamentals_part_1.js
@@ -24,3 +24,37 @@ const fizzBuzz = function(arr) {
 };
 
 console.log(fizzBuzz([1,3,5,10,15,21,30,42]));
+
+const isPrime = (number) => {
+  let i = 2;
+  while (i < number) {
+    if (number % i == 0) {
+      return false;
+    }
+    i++;
+  }
+  return true;
+};
+
+console.log(isPrime(2));
+console.log(isPrime(10));
+console.log(isPrime(15485863));
+console.log(isPrime(3548563));
+
+const sumOfNPrimes = (n) => {
+  let currentNumber = 2;
+  let sum = 0;
+  let count = 0;
+  while (count < n) {
+    if (isPrime(currentNumber)) {
+      sum += currentNumber;
+      count++;
+    }
+    currentNumber++;
+  }
+  return sum;
+};
+
+console.log(sumOfNPrimes(0));
+console.log(sumOfNPrimes(1));
+console.log(sumOfNPrimes(4));
