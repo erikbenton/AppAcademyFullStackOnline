f68c92a07e59669d861a8e055f10aa3b81779163
erikbenton <erik.benton@hotmail.com>
2020-03-23 11:48:17 -0400

    finishing first part of Pieces class

--- Javascript/JavaScript Fundamentals/reversi/lib/piece.js
+++ Javascript/JavaScript Fundamentals/reversi/lib/piece.js
@@ -1,26 +1,31 @@
 /**
  * Initializes the Piece with its color.
  */
-function Piece (color) {
+class Piece {
+  constructor(color) {
+    this.color = color;
+  }
+  /**
+   * Returns the color opposite the current piece.
+   */
+  oppColor() {
+  }
+  /**
+   * Changes the piece's color to the opposite color.
+   */
+  flip() {
+    this.color = this.color == "black" ? "white" : "black";
+  }
+  /**
+   * Returns a string representation of the string
+   * based on its color.
+   */
+  toString() {
+    return  (this.color == "black" ? "B" : "W");
+  }
 }
 
-/**
- * Returns the color opposite the current piece.
- */
-Piece.prototype.oppColor = function () {
-};
 
-/**
- * Changes the piece's color to the opposite color.
- */
-Piece.prototype.flip = function () {
-};
 
-/**
- * Returns a string representation of the string
- * based on its color.
- */
-Piece.prototype.toString = function () {
-};
 
 module.exports = Piece;
