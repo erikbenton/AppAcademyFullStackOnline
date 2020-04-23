0bef5df5ae6df52600a80a9ce2151d392d0dabd6
erikbenton <erik.benton@hotmail.com>
2020-04-23 17:42:13 -0400

    fixing promptMove so now it is recursive

--- Javascript/Event Loop/projects/towers_of_hanoi/game.js
+++ Javascript/Event Loop/projects/towers_of_hanoi/game.js
@@ -18,13 +18,9 @@ class Game {
       // Move the discs
   }
 
-  prompMove(callback) {
+  promptMove(callback) {
     reader.question("What are your 'From' and 'To' towers? [eg 1 3]\n", function(answer) {
-      if(callback(answer)) {
-        console.log("yay!")
-      } else {
-        console.log("Awww =(");
-      }
+      callback(answer);
     })
   }
 
@@ -47,8 +43,16 @@ class Game {
     }
   }
   
-  move(from, to) {
-
+  move() {
+    return (input) => {
+      if(this.isValidMove(input)) {
+        let moves = input.split(" ");
+        console.log("yay");
+      } else {
+        console.log("Not a valid move");
+      }
+      this.promptMove(this.move());
+    }
   }
 
   isValidMove(input) {
@@ -61,14 +65,14 @@ class Game {
       return false;
     }
 
-    inputArr.forEach(pos => {
-      if( !(pos instanceof Number) && pos > 2 && pos < 0) {
+    for(let i = 0; i < inputArr.length; i++) {
+      let pos = parseInt(inputArr[i]);
+      if( pos > 2 || pos < 0 || isNaN(pos)) {
         return false;
       }
-    })
+    }
 
     return true;
-
   }
 
   _isValidMove(from, to) {
@@ -84,4 +88,4 @@ class Game {
 let game = new Game();
 
 game.printStacks();
-game.prompMove(game.isValidMove);
+game.promptMove(game.move());
