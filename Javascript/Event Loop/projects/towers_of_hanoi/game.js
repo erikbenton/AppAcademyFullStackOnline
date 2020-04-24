8d2733db07a6eda6ac555be0ce45fea2451e4df9
erikbenton <erik.benton@hotmail.com>
2020-04-24 13:58:11 -0400

    finished Towers of Hanoi JS edition

--- Javascript/Event Loop/projects/towers_of_hanoi/game.js
+++ Javascript/Event Loop/projects/towers_of_hanoi/game.js
@@ -12,10 +12,8 @@ class Game {
   }
 
   run() {
-    // While the game isn't over
-      // Ask the user for a "From and To tower"
-      // Check if valid, report if not
-      // Move the discs
+    this.printStacks();
+    this.promptMove(game.move());
   }
 
   promptMove(callback) {
@@ -47,11 +45,18 @@ class Game {
     return (input) => {
       let moves = this.isValidMove(input)
       if(moves) {
-        console.log("yay");
+        this.towers[moves[0]][moves[3]] = 0;
+        this.towers[moves[1]][moves[4]] = moves[2];
+        this.printStacks();
       } else {
         console.log("Not a valid move");
       }
-      this.promptMove(this.move());
+      if( this._isOver() ) {
+        console.log("Yay, you won!");
+        reader.close();
+      } else {
+        this.promptMove(this.move());
+      }
     }
   }
 
@@ -67,43 +72,49 @@ class Game {
 
     let pos1 = parseInt(inputArr[0]) - 1;
     let pos2 = parseInt(inputArr[1]) - 1;
-    let val1, val2, i;
+    let val1, val2, ind1, ind2;
     
     if( (pos1 > 2 || pos1 < 0 || isNaN(pos1))
        || (this.towers[pos1][0] == 0)) {
       return false;
     } else {
-      i = 2;
-      val1 = this.towers[pos1][i];
-      while(this.towers[pos1][i] == 0 && i >= 0) {
-        val1 = this.towers[pos1][i];
-        i--;
+      ind1 = 2;
+      val1 = this.towers[pos1][ind1];
+      while(this.towers[pos1][ind1] == 0 && ind1 >= 0) {
+        ind1--;
+        val1 = this.towers[pos1][ind1];
       }
     }
-    if( pos2 > 2 || pos2 < 0 || isNaN(pos2)) {
+    if( pos2 > 2 || pos2 < 0 || isNaN(pos2)
+        || (this.towers[pos2][2] != 0) ) {
       return false;
     } else {
-      i = 2;
-      val2 = this.towers[pos2][i];
-      while(this.towers[pos2][i] == 0 && i >= 0) {
-        val2 = this.towers[pos2][i];
-        i--;
+      ind2 = 2;
+      val2 = this.towers[pos2][ind2];
+      while(this.towers[pos2][ind2] == 0 && ind2 >= 0) {
+        ind2--;
+        val2 = this.towers[pos2][ind2];
       }
       if( (val1 > val2) && val2 != 0) {
         return false;
       }
     }
 
-    return [pos1, pos2, val1, val2];
+    return [pos1, pos2, val1, ind1, ind2 + 1];
   }
 
   _isOver() {
-
+    if(this.towers[2][2] == 1
+    && this.towers[2][1] == 2
+    && this.towers[2][0] == 3 ) {
+      return true;
+    }
+    
+    return false
   }
 
 }
 
 let game = new Game();
 
-game.printStacks();
-game.promptMove(game.move());
+game.run();
