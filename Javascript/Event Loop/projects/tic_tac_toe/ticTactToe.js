085fe02553347b38e83188ce72581469377b45f0
erikbenton <erik.benton@hotmail.com>
2020-04-28 14:01:51 -0400

    adding Board class to JS TTT

--- Javascript/Event Loop/projects/tic_tac_toe/ticTactToe.js
+++ Javascript/Event Loop/projects/tic_tac_toe/ticTactToe.js
@@ -7,6 +7,30 @@ const reader = readline.createInterface({
 
 class Game {
   constructor() {
+    this.board = new Board();
+  }
+}
+
+class Board {
+  constructor() {
     this.grid = [["","",""],["","",""],["","",""]];
+  }
+
+  won() {
+
+  }
+
+  winner() {
+
+  }
+
+  empty(pos) {
+    return this.grid[pos[0]][pos[1]] === "";
+  }
+
+  placeMark(pos, mark) {
+    if(this.empty(pos)) {
+      this.grid[pos[0]][pos[1]] = mark;
+    }
   }
 }
