ce6ed05c557f262a0b5d4d4b3261d59e6281b20b
erikbenton <erik.benton@hotmail.com>
2020-06-02 16:43:10 -0700

    drawing green rectangle using canvas

--- Javascript/Object-oriented JS/Intro to Canvas/canvas_practice/practice.js
+++ Javascript/Object-oriented JS/Intro to Canvas/canvas_practice/practice.js
@@ -2,20 +2,8 @@ document.addEventListener("DOMContentLoaded", function(){
   const myCanvas = document.getElementById("mycanvas");
   const ctx = myCanvas.getContext('2d');
 
-  // Set line width
-ctx.lineWidth = 10;
+  ctx.fillStyle = "rgb(0, 255, 69)";
 
-// Wall
-ctx.strokeRect(75, 140, 150, 110);
+  ctx.fillRect(0, 0, 500, 500);
 
-// Door
-ctx.fillRect(130, 190, 40, 60);
-
-// Roof
-ctx.beginPath();
-ctx.moveTo(50, 140);
-ctx.lineTo(150, 60);
-ctx.lineTo(250, 140);
-ctx.closePath();
-ctx.stroke();
 });
