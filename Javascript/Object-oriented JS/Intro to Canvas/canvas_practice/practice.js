ca90cbb25fe72b42ffb88d57002e4f0f615f7889
erikbenton <erik.benton@hotmail.com>
2020-06-02 16:36:04 -0700

    starting Intro to Canvas

---
+++ Javascript/Object-oriented JS/Intro to Canvas/canvas_practice/practice.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const myCanvas = document.getElementById("mycanvas");
+  const ctx = myCanvas.getContext('2d');
+
+  // Set line width
+ctx.lineWidth = 10;
+
+// Wall
+ctx.strokeRect(75, 140, 150, 110);
+
+// Door
+ctx.fillRect(130, 190, 40, 60);
+
+// Roof
+ctx.beginPath();
+ctx.moveTo(50, 140);
+ctx.lineTo(150, 60);
+ctx.lineTo(250, 140);
+ctx.closePath();
+ctx.stroke();
+});
